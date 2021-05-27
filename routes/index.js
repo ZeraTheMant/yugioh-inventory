3f85ea084f5cb1f3e99d44d5658ac8c358bfc2a1
unknown <imbapalagz@gmail.com>
2021-05-27 11:18:03 +0800

    added nodemon to package.json, still working on routes

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ const spell_card_controller = require('../controllers/spellCardController');
 const trap_card_controller = require('../controllers/trapCardController');
 const all_cards_controller = require('../controllers/allCardsController');
 
-
 /* index page. */
-router.get('/', function(req, res, next) {
-	res.render('index', all_cards_controller.index);
-});
+router.get('/', all_cards_controller.index);
+
+
+
 
 // attribute routes start //
 router.get('/attribute/create', attribute_controller.attribute_create_get);
@@ -24,7 +24,62 @@ router.post('/attribute/create', attribute_controller.attribute_create_post);
 router.get('/attribute/:id/delete', attribute_controller.attribute_delete_get);
 router.post('/attribute/:id/delete', attribute_controller.attribute_delete_post);
 
-router.get('/attribu
+router.get('/attribute/:id/update', attribute_controller.attribute_update_get);
+router.post('/attribute/:id/update', attribute_controller.attribute_update_post);
+
+router.get('/attribute/:id', attribute_controller.attribute_detail);
+router.get('/attributes/', attribute_controller.attribute_list);
 // attribute routes end //
+
+
+
+
+// monster category routes start //
+router.get('/monster_category/create', monster_category_controller.monster_category_create_get);
+router.post('/monster_category/create', monster_category_controller.monster_category_create_post);
+
+router.get('/monster_category/:id/delete', monster_category_controller.monster_category_delete_get);
+router.post('/monster_category/:id/delete', monster_category_controller.monster_category_delete_post);
+
+router.get('/monster_category/:id/update', monster_category_controller.monster_category_update_get);
+router.post('/monster_category/:id/update', monster_category_controller.monster_category_update_post);
+
+router.get('/monster_category/:id', monster_category_controller.monster_category_detail);
+router.get('/monster_categories/', monster_category_controller.monster_category_list);
+// monster category routes end //
+
+
+
+
+// monster type routes start //
+router.get('/monster_type/create', monster_type_controller.monster_type_create_get);
+router.post('/monster_type/create', monster_type_controller.monster_type_create_post);
+
+router.get('/monster_type/:id/delete', monster_type_controller.monster_type_delete_get);
+router.post('/monster_type/:id/delete', monster_type_controller.monster_type_delete_post);
+
+router.get('/monster_type/:id/update', monster_type_controller.monster_type_update_get);
+router.post('/monster_type/:id/update', monster_type_controller.monster_type_update_post);
+
+router.get('/monster_type/:id', monster_type_controller.monster_type_detail);
+router.get('/monster_types/', monster_type_controller.monster_type_list);
+// monster type routes end //
+
+
+
+
+// monster card routes start //
+router.get('/monster_type/create', monster_type_controller.monster_type_create_get);
+router.post('/monster_type/create', monster_type_controller.monster_type_create_post);
+
+router.get('/monster_type/:id/delete', monster_type_controller.monster_type_delete_get);
+router.post('/monster_type/:id/delete', monster_type_controller.monster_type_delete_post);
+
+router.get('/monster_type/:id/update', monster_type_controller.monster_type_update_get);
+router.post('/monster_type/:id/update', monster_type_controller.monster_type_update_post);
+
+router.get('/monster_type/:id', monster_type_controller.monster_type_detail);
+router.get('/monster_cards/', monster_type_controller.monster_type_list);
+// monster card routes end //
 
 module.exports = router;
